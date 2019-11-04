e16b72bf5e8f469063202f5c53f4a78e92465236
Aditya Hajare <aditya43@gmail.com>
2019-11-04 19:33:30 +0530

    Added route group for admin routes

--- 3-Express.js/routes/admin.js
+++ 3-Express.js/routes/admin.js
@@ -3,13 +3,13 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/add-product', (request, response, next) => {
-    response.send(`<form action='/product' method='POST'>
+    response.send(`<form action='/admin/add-product' method='POST'>
         <input type='text' name='product-name' />
         <button type='submit'>Submit</button>
     </form>`);
 });
 
-router.post('/product', (request, response, next) => {
+router.post('/add-product', (request, response, next) => {
     console.log(request.body);
     response.redirect('/');
 });
