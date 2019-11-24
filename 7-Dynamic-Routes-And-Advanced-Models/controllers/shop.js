4fef7d43d49067cbc546b6aa8beede2438d1e2f7
Aditya Hajare <aditya43@gmail.com>
2019-11-24 20:22:07 +0530

    Added product id in route URL

--- 7-Dynamic-Routes-And-Advanced-Models/controllers/shop.js
+++ 7-Dynamic-Routes-And-Advanced-Models/controllers/shop.js
@@ -1,42 +1,48 @@
 const Product = require('../models/product');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/product-list', {
-      prods: products,
-      pageTitle: 'All Products',
-      path: '/products'
+    Product.fetchAll(products => {
+        res.render('shop/product-list', {
+            prods: products,
+            pageTitle: 'All Products',
+            path: '/products'
+        });
     });
-  });
+};
+
+exports.getProduct = (req, res, next) => {
+    const id = req.params.productId;
+    console.log(id);
+    res.redirect('/');
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/index', {
-      prods: products,
-      pageTitle: 'Shop',
-      path: '/'
+    Product.fetchAll(products => {
+        res.render('shop/index', {
+            prods: products,
+            pageTitle: 'Shop',
+            path: '/'
+        });
     });
-  });
 };
 
 exports.getCart = (req, res, next) => {
-  res.render('shop/cart', {
-    path: '/cart',
-    pageTitle: 'Your Cart'
-  });
+    res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart'
+    });
 };
 
 exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', {
-    path: '/orders',
-    pageTitle: 'Your Orders'
-  });
+    res.render('shop/orders', {
+        path: '/orders',
+        pageTitle: 'Your Orders'
+    });
 };
 
 exports.getCheckout = (req, res, next) => {
-  res.render('shop/checkout', {
-    path: '/checkout',
-    pageTitle: 'Checkout'
-  });
+    res.render('shop/checkout', {
+        path: '/checkout',
+        pageTitle: 'Checkout'
+    });
 };
