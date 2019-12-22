9294f08c9b748c5526d83e784ff18cf2525632b8
Aditya Hajare <aditya43@gmail.com>
2019-12-22 19:59:33 +0530

    Add to cart

--- 7-Dynamic-Routes-And-Advanced-Models/controllers/shop.js
+++ 7-Dynamic-Routes-And-Advanced-Models/controllers/shop.js
@@ -31,6 +31,12 @@ exports.getIndex = (req, res, next) => {
     });
 };
 
+exports.postCart = (req, res, next) => {
+    const prodId = req.body.productId;
+    console.log(prodId);
+    res.redirect('/cart');
+};
+
 exports.getCart = (req, res, next) => {
     res.render('shop/cart', {
         path: '/cart',
