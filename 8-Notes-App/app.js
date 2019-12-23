b4ce8c1a1feafbe71d57f3039847f97ccdb8e001
Aditya Hajare <aditya43@gmail.com>
2019-12-23 08:22:48 +0530

    Notes App - Parsing arguments with Yarg - II

--- 8-Notes-App/app.js
+++ 8-Notes-App/app.js
@@ -7,8 +7,21 @@ yargs.version('1.1.0');
 yargs.command({
     command: 'add',
     description: 'Add a new note',
-    handler: () => {
-        console.log('Adding a new note');
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: (argv) => {
+        console.log(`Title: ${argv.title}`);
+        console.log(`Title: ${argv.body}`);
     }
 });
 
@@ -39,4 +52,4 @@ yargs.command({
     }
 });
 
-console.log(yargs.argv);
+yargs.parse();
