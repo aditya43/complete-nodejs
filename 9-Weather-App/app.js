c9716ad3ad5c2b48c12e2da8db00bcad4b455083
Aditya Hajare <aditya43@gmail.com>
2019-12-24 20:07:50 +0530

    Weather App - Geaocode Utils | Bad Way | Callbacks

--- 9-Weather-App/app.js
+++ 9-Weather-App/app.js
@@ -1,38 +1,43 @@
-const request = require('request');
+const geocode = require('./utils/geocode');
 
-const weatherUrl = 'https://api.darksky.net/forecast/b597e671b6449d2353f84da5febd0ce2/37.8267,-122.4233';
-const mapLatLongUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Pune%20Koregaon%20Park.json?access_token=pk.eyJ1IjoiYWRpdHlhNDMiLCJhIjoiY2s0anJ2M3o2MXQxZTNscXFyenkzc3IwMCJ9.jY6wENlkRl2TIcZ3SV6XVQ&limit=1';
+// const weatherUrl = 'https://api.darksky.net/forecast/b597e671b6449d2353f84da5febd0ce2/37.8267,-122.4233';
+// const mapLatLongUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Pune%20Koregaon%20Park.json?access_token=pk.eyJ1IjoiYWRpdHlhNDMiLCJhIjoiY2s0anJ2M3o2MXQxZTNscXFyenkzc3IwMCJ9.jY6wENlkRl2TIcZ3SV6XVQ&limit=1';
 
-request({ url: weatherUrl, json: true }, (error, response) => {
-    if (error) {
-        console.log('Unable to connect to weather service!');
-    }
+// request({ url: weatherUrl, json: true }, (error, response) => {
+//     if (error) {
+//         console.log('Unable to connect to weather service!');
+//     }
 
-    if (response.body.code) {
-        console.log(`${response.body.code}: ${response.body.error}`);
-    }
+//     if (response.body.code) {
+//         console.log(`${response.body.code}: ${response.body.error}`);
+//     }
 
-    if (!error && response.body.daily) {
-        const res = response.body;
+//     if (!error && response.body.daily) {
+//         const res = response.body;
 
-        console.log(`${res.daily.data[0].summary}. It is currently ${res.currently.temperature} degrees out. There is a ${res.currently.precipProbability}% chance of rain right now.`);
-    }
-});
+//         console.log(`${res.daily.data[0].summary}. It is currently ${res.currently.temperature} degrees out. There is a ${res.currently.precipProbability}% chance of rain right now.`);
+//     }
+// });
 
-request({ url: mapLatLongUrl, json: true }, (error, response) => {
-    if (error) {
-        console.log('Unable to connect to Mapbox service!');
-    }
+// request({ url: mapLatLongUrl, json: true }, (error, response) => {
+//     if (error) {
+//         console.log('Unable to connect to Mapbox service!');
+//     }
 
-    if (response.body.features.length === 0) {
-        console.log('Unable to find this location. Please try different location!');
-    }
+//     if (response.body.features.length === 0) {
+//         console.log('Unable to find this location. Please try different location!');
+//     }
 
-    if (!error && response.body.features.length > 0) {
-        const latitude = response.body.features[0].center[1];
-        const longitude = response.body.features[0].center[0];
+//     if (!error && response.body.features.length > 0) {
+//         const latitude = response.body.features[0].center[1];
+//         const longitude = response.body.features[0].center[0];
 
-        console.dir(latitude);
-        console.dir(longitude);
-    }
+//         console.dir(latitude);
+//         console.dir(longitude);
+//     }
+// });
+
+geocode('Pune Koregaon Park', (error, data) => {
+    console.log(error);
+    console.log(data);
 });
---
+++ 9-Weather-App/utils/geocode.js
@@ -0,0 +1,21 @@
+const request = require('request');
+
+const geocode = (address, callback) => {
+    const location = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=pk.eyJ1IjoiYWRpdHlhNDMiLCJhIjoiY2s0anJ2M3o2MXQxZTNscXFyenkzc3IwMCJ9.jY6wENlkRl2TIcZ3SV6XVQ&limit=1`;
+
+    request({ url: location, json: true }, (error, response) => {
+        if (error) {
+            callback('Unable to connect to location service', undefined);
+        } else if (response.body.features.length === 0) {
+            callback('Unable to fine location, try another search!', undefined);
+        } else {
+            callback(undefined, {
+                latitude: response.body.features[0].center[1],
+                longitude: response.body.features[0].center[0],
+                location: response.body.features[0].place_name
+            });
+        }
+    });
+};
+
+module.exports = geocode;
