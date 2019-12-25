e165d32000840cf34ee8e2ddfcfdac44d96b7522
Aditya Hajare <aditya43@gmail.com>
2019-12-25 17:15:56 +0530

    Weather App - ExpressJS | Basics | 404 Pages

--- 10-Weather-App-Express/src/app.js
+++ 10-Weather-App-Express/src/app.js
@@ -10,6 +10,13 @@ app.use(express.static(path.join(__dirname, '../public'))); // Set default path 
 app.set('views', path.join(__dirname, '../templates/views')); // Set default path for views.
 hbs.registerPartials(path.join(__dirname, '../templates/partials')); // Set default path for views partials.
 
+app.get('', (req, res) => {
+    res.render('index', {
+        pageTitle: 'Weather App',
+        name: 'Aditya Hajare'
+    });
+});
+
 app.get('/weather', (req, res) => {
     res.send({ // Sending JSON.
         forecast: 'It is raining',
@@ -32,9 +39,21 @@ app.get('/help', (req, res) => {
     });
 });
 
-app.get('', (req, res) => {
-    res.render('index', {
-        pageTitle: 'Weather App',
+app.get('/help/*', (req, res) => {
+    // To handle help specific 404 pages using Express wildcard '*'.
+    res.render('404', {
+        errorMsg: 'Help article not found',
+        pageTitle: 'Help Page',
+        name: 'Aditya Hajare'
+    });
+});
+
+app.get('*', (req, res) => {
+    // Express provides wildcard character '*' to match everything that hasn't been matched so far.
+    // This is useful for 404.
+    res.render('404', {
+        errorMsg: '404 Page Not Found!',
+        pageTitle: '404 - Page Not Found',
         name: 'Aditya Hajare'
     });
 });
