65702b47325a55fba751375fdf7a48fdc053d1f2
Aditya Hajare <aditya43@gmail.com>
2019-12-25 12:55:32 +0530

    Weather App - ExpressJS | Basics | Templating Engine - Handlebars Setup And Views

--- 10-Weather-App-Express/src/app.js
+++ 10-Weather-App-Express/src/app.js
@@ -3,12 +3,34 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, '../public'))); // Static Assets
 
 app.get('/weather', (req, res) => {
     res.send({ // Sending JSON
         forecast: 'It is raining',
         location: 'Koregaon Park, Pune'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        pageTitle: 'Weather App About Page',
+        name: 'Aditya Hajare'
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        pageTitle: 'Help Page',
+        message: 'This is a sample help message'
+    });
+});
+
+app.get('', (req, res) => {
+    res.render('index', {
+        pageTitle: 'Weather App',
+        name: 'Aditya Hajare'
     });
 });
 
