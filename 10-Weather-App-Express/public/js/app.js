e356ef0d003fde48232f7ba1b9520960ecb4b77f
Aditya Hajare <aditya43@gmail.com>
2019-12-25 19:14:52 +0530

    Weather App - Express | AJAX Request using Fetch

--- 10-Weather-App-Express/public/js/app.js
+++ 10-Weather-App-Express/public/js/app.js
@@ -1,1 +1,11 @@
 console.log('Client side javascript is loaded');
+
+fetch('http://puzzle.mead.io/puzzle')
+    .then(res => {
+        res.json().then(data => {
+            console.dir(data);
+        });
+    })
+    .catch(err => {
+        console.dir(err);
+    });
