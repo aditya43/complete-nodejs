68e8985cbabc76f3aa0d79dacd8b93b2278bd039
Aditya Hajare <aditya43@gmail.com>
2019-12-26 15:37:50 +0530

    Weather App | Make app ready for deployment on Heroku. App URL: https://aditya-hajare-weather-app.herokuapp.com/

--- 10-Weather-App-Express/public/js/app.js
+++ 10-Weather-App-Express/public/js/app.js
@@ -8,7 +8,7 @@ if (document.querySelector('form')) {
 
         const location = document.querySelector('input').value;
 
-        fetch(`http://localhost:3000/weather?address=${location}`)
+        fetch(`/weather?address=${location}`)
             .then(res => {
                 res.json().then(data => {
                     if (data.error) {
