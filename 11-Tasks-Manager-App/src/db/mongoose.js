ad413e00fd9fd11f58529eaaedd76fa3705aa9c1
Aditya Hajare <aditya43@gmail.com>
2019-12-27 21:50:12 +0530

    Task Manager App | MongoDB | Data validation and sanitization

--- 11-Tasks-Manager-App/src/db/mongoose.js
+++ 11-Tasks-Manager-App/src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const mongoDB = 'mongodb://127.0.0.1:27017/task-manager-app';
 
@@ -15,39 +16,53 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-const Task = mongoose.model('Task', {
-    description: {
-        type: String
-    },
-    completed: {
-        type: Boolean
-    }
-});
-
-const task = new Task({
-    description: 'Sample Task 6',
-    completed: true
-});
-
-task.save().then(res => console.log(res)).catch(err => console.log(err));
-
-// const User = mongoose.model('User', {
-//     name: {
+// const Task = mongoose.model('Task', {
+//     description: {
 //         type: String
 //     },
-//     age: {
-//         type: Number
+//     completed: {
+//         type: Boolean
 //     }
 // });
 
-// const jane = new User({
-//     name: 'Jane Doe',
-//     age: 35
+// const task = new Task({
+//     description: 'Sample Task 6',
+//     completed: true
 // });
 
-// jane.save()
-//     .then(res => {
-//         console.log(res);
-//     }).catch(err => {
-//         console.log(err);
-//     });
+// task.save().then(res => console.log(res)).catch(err => console.log(err));
+
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate (value) {
+            if (value < 0) {
+                throw new Error('Age must be positive number');
+            }
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate (value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Invalid email address');
+            }
+        }
+    }
+});
+
+const jane = new User({
+    name: 'Jane Doe',
+    age: 35
+});
+
+jane.save().then(res => console.log(res)).catch(err => console.log(err));
