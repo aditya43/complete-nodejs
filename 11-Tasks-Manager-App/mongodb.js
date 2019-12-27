d0a965c29b6b48c206c9857e8eefe57e34d41949
Aditya Hajare <aditya43@gmail.com>
2019-12-27 07:35:54 +0530

    Task Manager App | MongoDB Basic Inserting A Sample Record

--- 11-Tasks-Manager-App/mongodb.js
+++ 11-Tasks-Manager-App/mongodb.js
@@ -2,12 +2,17 @@ const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
 const connectionUrl = 'mongodb://127.0.0.1:27017';
-// const databaseName = 'task-manager-app';
+const databaseName = 'task-manager-app';
 
 MongoClient.connect(connectionUrl, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
     if (error) {
         console.log('Unable to connect to database!', error);
     }
 
-    console.log('Connected Successfully!');
+    const db = client.db(databaseName);
+
+    db.collection('users').insertOne({
+        name: 'Aditya Hajare',
+        age: 32
+    });
 });
