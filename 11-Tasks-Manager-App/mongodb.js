2853a134ba756c5d6910917211c97a74190227c9
Aditya Hajare <aditya43@gmail.com>
2019-12-27 07:24:29 +0530

    Task Manager App | MongoDB Basic Connection

--- 11-Tasks-Manager-App/mongodb.js
+++ 11-Tasks-Manager-App/mongodb.js
@@ -2,11 +2,11 @@ const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
 const connectionUrl = 'mongodb://127.0.0.1:27017';
-const databaseName = 'task-manager-app';
+// const databaseName = 'task-manager-app';
 
-MongoClient.connect(connectionUrl, { useNewUrlParser: true }, (error, client) => {
+MongoClient.connect(connectionUrl, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
     if (error) {
-        console.log('Unable to connect to database!');
+        console.log('Unable to connect to database!', error);
     }
 
     console.log('Connected Successfully!');
