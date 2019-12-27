bb0b2bdcfc2da7a69e08bae9e5052a374e9cbd4c
Aditya Hajare <aditya43@gmail.com>
2019-12-27 07:48:47 +0530

    Task Manager App | MongoDB Basic Insert Many

--- 11-Tasks-Manager-App/mongodb.js
+++ 11-Tasks-Manager-App/mongodb.js
@@ -11,8 +11,31 @@ MongoClient.connect(connectionUrl, { useNewUrlParser: true, useUnifiedTopology: 
 
     const db = client.db(databaseName);
 
-    db.collection('users').insertOne({
-        name: 'Aditya Hajare',
-        age: 32
+    // db.collection('users').insertOne({
+    //     name: 'Aditya Hajare',
+    //     age: 32
+    // }, (error, res) => {
+    //     if (error) {
+    //         return console.log('Unable to insert user');
+    //     }
+
+    //     console.log(res);
+    // });
+
+    db.collection('tasks').insertMany([{
+        description: 'Sample Task 1',
+        completed: false
+    }, {
+        description: 'Sample Task 2',
+        completed: true
+    }, {
+        description: 'Sample Task 3',
+        completed: false
+    }], (error, res) => {
+        if (error) {
+            return console.log('Failed to insert docs', error);
+        }
+
+        console.log(res.ops);
     });
 });
