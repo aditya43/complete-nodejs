25631049a2b399b7dcc5f66c92130544a6505f57
Aditya Hajare <aditya43@gmail.com>
2019-12-27 15:19:34 +0530

    Task Manager App | MongoDB | Basics | update And updateMany

--- 11-Tasks-Manager-App/mongodb.js
+++ 11-Tasks-Manager-App/mongodb.js
@@ -1,8 +1,4 @@
-// const mongodb = require('mongodb');
-// const MongoClient = mongodb.MongoClient;
-// const ObjectID = mongodb.ObjectID;
-
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectID } = require('mongodb');
 
 const connectionUrl = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager-app';
@@ -14,43 +10,27 @@ MongoClient.connect(connectionUrl, { useNewUrlParser: true, useUnifiedTopology: 
 
     const db = client.db(databaseName);
 
-    // db.collection('users').insertOne({
-    //     name: 'Aditya Hajare',
-    //     age: 32
-    // }, (error, res) => {
-    //     if (error) {
-    //         return console.log('Unable to insert user');
+    // db.collection('tasks').updateOne({
+    //     _id: new ObjectID('5e05a91616e9b93470c5ab1c')
+    // }, {
+    //     $set: {
+    //         completed: true
     //     }
-
-    //     console.log(res);
+    // }).then(res => {
+    //     console.log(res)
+    // }).catch(err => {
+    //     console.log(err)
     // });
 
-    // db.collection('tasks').insertMany([{
-    //     description: 'Sample Task 1',
-    //     completed: false
-    // }, {
-    //     description: 'Sample Task 2',
-    //     completed: true
-    // }, {
-    //     description: 'Sample Task 3',
-    //     completed: false
-    // }], (error, res) => {
-    //     if (error) {
-    //         return console.log('Failed to insert docs', error);
-    //     }
-
-    //     console.log(res.ops);
-    // });
-
-    db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
-        console.log(tasks);
-    })
-
-    db.collection('tasks').findOne({ description: 'Sample Task 2' }, (error, task) => {
-        if (error) {
-            console.log('Failed to fetch task', error);
+    db.collection('tasks').updateMany({
+        completed: false
+    }, {
+        $set: {
+            completed: true
         }
-
-        console.log(task);
-    })
+    }).then(({result}) => {
+        console.log(result)
+    }).catch(err => {
+        console.log(err);
+    });
 });
