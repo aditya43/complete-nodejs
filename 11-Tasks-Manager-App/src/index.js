19d8d56219b06a5b79559b56f95bdb2d77df13ef
Aditya Hajare <aditya43@gmail.com>
2019-12-28 09:59:56 +0530

    Task Manager App | MongoDB | Get User By Id And Get All Users API

--- 11-Tasks-Manager-App/src/index.js
+++ 11-Tasks-Manager-App/src/index.js
@@ -20,6 +20,28 @@ app.post('/users', (req, res) => {
     });
 });
 
+app.get('/users', (req, res) => {
+    User.find().then(users => {
+        res.send(users);
+    }).catch(err => {
+        res.status(500).send(err);
+    });
+});
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id;
+
+    User.findById(_id).then(user => {
+        if (!user) {
+            return res.status(404).send({});
+        }
+
+        res.status(200).send(user);
+    }).catch(err => {
+        res.status(500).send(err);
+    });
+});
+
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body);
 
