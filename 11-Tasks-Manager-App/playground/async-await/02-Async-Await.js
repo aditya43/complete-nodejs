e88b6dfd09e4697b0e29352e160d64543fb7c122
Aditya Hajare <aditya43@gmail.com>
2019-12-28 17:35:21 +0530

    Task Manager App | Playground | Async Await | Async Await Functions

---
+++ 11-Tasks-Manager-App/playground/async-await/02-Async-Await.js
@@ -0,0 +1,22 @@
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (a < 0 || b < 0) {
+                return reject('Please provide positive numbers only');
+            }
+            resolve(a + b);
+        }, 500);
+    });
+};
+
+const foo = async () => {
+    const sum1 = await add(1, 1);
+    const sum2 = await add(sum1, 1);
+    const sum3 = await add(sum2, 1);
+
+    return sum3;
+};
+
+foo()
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
