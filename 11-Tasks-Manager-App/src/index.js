c38fc119167377a0e38025a659481a34cfcb5602
Aditya Hajare <aditya43@gmail.com>
2019-12-28 17:58:35 +0530

    Task Manager App | Playground | Async Await | Async Await Functions | Users Functions

--- 11-Tasks-Manager-App/src/index.js
+++ 11-Tasks-Manager-App/src/index.js
@@ -10,36 +10,40 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     const user = new User(req.body);
 
-    user.save().then(() => {
+    try {
+        await user.save();
         res.status(201).send(user);
-    }).catch(err => {
-        res.status(400).send(err);
-    });
+    } catch (e) {
+        res.status(400).send(e);
+    }
 });
 
-app.get('/users', (req, res) => {
-    User.find().then(users => {
-        res.send(users);
-    }).catch(err => {
-        res.status(500).send(err);
-    });
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).send(users);
+    } catch (e) {
+        res.status(500).send(e);
+    }
 });
 
-app.get('/users/:id', (req, res) => {
-    const _id = req.params.id;
+app.get('/users/:id', async (req, res) => {
+    try {
+        const _id = req.params.id;
 
-    User.findById(_id).then(user => {
+        const user = await User.findById(_id);
+
         if (!user) {
             return res.status(404).send({});
         }
 
         res.status(200).send(user);
-    }).catch(err => {
-        res.status(500).send(err);
-    });
+    } catch (e) {
+        res.status(500).send(e);
+    }
 });
 
 app.post('/tasks', (req, res) => {
