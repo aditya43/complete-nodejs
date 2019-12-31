25091d7f37ca0ac79439bba22998e95b90dd0ac2
Aditya Hajare <aditya43@gmail.com>
2019-12-31 06:35:26 +0530

    Task Manager App | Set User Avatar | Restrict File Size And Type

--- 11-Tasks-Manager-App/src/routes/users.js
+++ 11-Tasks-Manager-App/src/routes/users.js
@@ -14,7 +14,19 @@ router.post('/users/login', userController.login);
 router.post('/users/logout', authMiddleware, userController.logout);
 router.post('/users/logoutAll', authMiddleware, userController.logoutAll);
 
-const upload = multer({ dest: 'avatars' });
+const upload = multer({
+    dest: 'avatars',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter (req, file, cb) {
+        if (!file.originalname.match(/\.(doc|docx)$/)) {
+            return cb(new Error('Please upload a word document'));
+        }
+
+        cb(undefined, true);
+    }
+});
 router.post('/users/me/avatar', upload.single('avatar'), userController.setAvatar);
 
 module.exports = router;
