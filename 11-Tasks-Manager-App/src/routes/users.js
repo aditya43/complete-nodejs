eb8ff1496b626d5fa149494b3e6af541be1a80ad
Aditya Hajare <aditya43@gmail.com>
2019-12-31 06:37:13 +0530

    Task Manager App | Set User Avatar | Restrict File Size And Type | 1mb, jpeg, jpg, png

--- 11-Tasks-Manager-App/src/routes/users.js
+++ 11-Tasks-Manager-App/src/routes/users.js
@@ -20,7 +20,7 @@ const upload = multer({
         fileSize: 1000000
     },
     fileFilter (req, file, cb) {
-        if (!file.originalname.match(/\.(doc|docx)$/)) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
             return cb(new Error('Please upload a word document'));
         }
 
