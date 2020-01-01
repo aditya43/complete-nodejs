022ba28f1442c50349c6b08a00d122017333975c
Aditya Hajare <aditya43@gmail.com>
2020-01-01 13:59:25 +0530

    Jest | Tasks API Testing | should create task for user

--- 11-Tasks-Manager-App/tests/task.test.js
+++ 11-Tasks-Manager-App/tests/task.test.js
@@ -6,6 +6,15 @@ const { userOneId, userOne, populateDatabase } = require('./fixtures/db');
 
 beforeEach(populateDatabase);
 
-test('should create task for user', () => {
+test('should create task for user', async () => {
+    const response = await request(app)
+        .post('/tasks')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            description: 'From Jest test'
+        })
+        .expect(201);
 
+    const task = Task.findById(response.body._id);
+    expect(task).not.toBeNull();
 });
