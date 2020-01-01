4d34cd714ad1b4b873e20a10fb5260c0965681ce
Aditya Hajare <aditya43@gmail.com>
2020-01-01 19:45:58 +0530

    Socket.io Chat App | Form and button states

--- 12-Socket-Chat-App/public/js/chat.js
+++ 12-Socket-Chat-App/public/js/chat.js
@@ -1,15 +1,25 @@
 const socket = io();
 
+$messageForm = document.querySelector('#message-form');
+$messageFormInput = document.querySelector('#input');
+$messageFormButton = document.querySelector('#button');
+$sendLocationButton = document.querySelector('#send-location');
+
 socket.on('message', message => {
     console.log(message);
 });
 
-document.querySelector('#message-form').addEventListener('submit', e => {
+$messageForm.addEventListener('submit', e => {
     e.preventDefault();
+    $messageFormButton.setAttribute('disabled', 'disabled');
 
     const message = e.target.elements.message.value;
 
     socket.emit('sendMessage', message, error => {
+        $messageFormButton.removeAttribute('disabled');
+        $messageFormInput.value = '';
+        $messageFormInput.focus();
+
         if (error) {
             console.log('Bad word detected!');
         }
@@ -18,16 +28,19 @@ document.querySelector('#message-form').addEventListener('submit', e => {
     });
 });
 
-document.querySelector('#send-location').addEventListener('click', e => {
+$sendLocationButton.addEventListener('click', e => {
     if (!navigator.geolocation) {
         return alert('Geolocation is not supported by your browser!');
     }
+
+    $sendLocationButton.setAttribute('disabled', 'disabled');
 
     navigator.geolocation.getCurrentPosition(position => {
         socket.emit('sendLocation', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         }, () => {
+            $sendLocationButton.removeAttribute('disabled');
             console.log('Location shared!');
         });
     });
