38749487fc1de89aaa322594634f1c6a9189989c
Aditya Hajare <aditya43@gmail.com>
2020-01-01 09:06:06 +0530

    Jest | Examples | Testing basic maths functions

--- 11-Tasks-Manager-App/tests/math.test.js
+++ 11-Tasks-Manager-App/tests/math.test.js
@@ -1,7 +1,11 @@
-test('Hello World!', () => {
+const { calculateTip } = require('../src/math');
 
+test('should calculate total with tip', () => {
+    const total = calculateTip(10, 0.3);
+    expect(total).toBe(13);
 });
 
-test('This should fail! ', () => {
-    throw new Error('This will fail!');
+test('should calculate total with default tip', () => {
+    const total = calculateTip(10);
+    expect(total).toBe(12.5);
 });
