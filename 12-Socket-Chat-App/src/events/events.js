ee2457317ec826f0ff39bd02b37394c198f42d67
Aditya Hajare <aditya43@gmail.com>
2020-01-02 15:45:01 +0530

    Socket.io Chat App | Tracking users joining and leaving chat rooms.

--- 12-Socket-Chat-App/src/events/events.js
+++ 12-Socket-Chat-App/src/events/events.js
@@ -1,14 +1,22 @@
 const Filter = require('bad-words');
 const { generateMessage, generateLocationMessage } = require('../utils/messages');
+const { addUser, removeUser, getUser, getUsersInRoom } = require('../utils/users');
 
 exports.newConnection = (socket, io) => {
     console.log('New websocket connection');
 
-    socket.on('join', ({ username, room }) => {
-        socket.join(room);
+    socket.on('join', ({ username, room }, callback) => {
+        const { error, user } = addUser({ id: socket.id, username, room });
+
+        if (error) {
+            return callback(error);
+        }
+
+        socket.join(user.room);
 
         socket.emit('message', generateMessage('Welcome!'));
-        socket.broadcast.to(room).emit('message', generateMessage(`${username} has joined!`));
+        socket.broadcast.to(user.room).emit('message', generateMessage(`${user.username} has joined!`));
+        callback();
     });
 
     socket.on('sendMessage', (message, callback) => {
@@ -28,6 +36,10 @@ exports.newConnection = (socket, io) => {
     });
 
     socket.on('disconnect', () => {
-        io.emit('message', generateMessage('A user has left!'));
+        const user = removeUser(socket.id);
+
+        if (user) {
+            io.to(user.room).emit('message', generateMessage(`${user.username} has left!`));
+        }
     });
 };
