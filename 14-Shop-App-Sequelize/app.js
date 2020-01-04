8335d4b6a52a666282f485bffad0236ea2dcabe9
Aditya Hajare <aditya43@gmail.com>
2020-01-04 18:05:41 +0530

    Shop App - Sequelize | hasMany And belongsTo relationships

--- 14-Shop-App-Sequelize/app.js
+++ 14-Shop-App-Sequelize/app.js
@@ -6,6 +6,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const sequelize = require('./util/database');
 
+const Product = require('./models/product');
+const User = require('./models/user');
+
 const errorController = require('./controllers/error');
 
 const app = express();
@@ -21,10 +24,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-
 app.use(errorController.get404);
 
-sequelize.sync()
+Product.belongsTo(User, {
+    constraints: true,
+    onDelete: 'CASCADE'
+});
+User.hasMany(Product);
+
+sequelize.sync({ force: true })
     .then(res => {
         app.listen(process.env.PORT, () => console.log(`Server is running on ${process.env.PORT}`));
     })
