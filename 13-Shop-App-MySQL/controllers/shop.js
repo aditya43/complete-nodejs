2138e68aabe41f302139d6f3f2441bef96ca6541
Aditya Hajare <aditya43@gmail.com>
2020-01-04 12:40:07 +0530

    Shop App - MySQL | Get single product

--- 13-Shop-App-MySQL/controllers/shop.js
+++ 13-Shop-App-MySQL/controllers/shop.js
@@ -15,13 +15,15 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
     const prodId = req.params.productId;
-    Product.findById(prodId, product => {
-        res.render('shop/product-detail', {
-            product: product,
-            pageTitle: product.title,
-            path: '/products'
-        });
-    });
+    Product.findById(prodId)
+        .then(([product]) => {
+            res.render('shop/product-detail', {
+                product: product[0],
+                pageTitle: product[0].title,
+                path: '/products'
+            });
+        })
+        .catch(e => console.log(e));
 };
 
 exports.getIndex = (req, res, next) => {
--- 13-Shop-App-MySQL/models/product.js
+++ 13-Shop-App-MySQL/models/product.js
@@ -23,6 +23,6 @@ module.exports = class Product {
     }
 
     static findById (id) {
-        //
+        return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
     }
 };
