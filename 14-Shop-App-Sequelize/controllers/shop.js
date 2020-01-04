8a5a621d1618311603eac0f9dd27d3992c2ce8cc
Aditya Hajare <aditya43@gmail.com>
2020-01-04 15:36:34 +0530

    Shop App - Sequelize | Get Single Product And Get All Products

--- 14-Shop-App-Sequelize/controllers/shop.js
+++ 14-Shop-App-Sequelize/controllers/shop.js
@@ -2,15 +2,13 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-    Product.fetchAll()
-        .then(([rows, fieldData]) => {
-            res.render('shop/product-list', {
-                prods: rows,
-                pageTitle: 'All Products',
-                path: '/products'
-            });
-        })
-        .catch(e => console.log(e));
+    Product.findAll().then(products => {
+        res.render('shop/product-list', {
+            prods: products,
+            pageTitle: 'All Products',
+            path: '/products'
+        });
+    }).catch(e => console.log(e));
 };
 
 exports.getProduct = (req, res, next) => {
@@ -27,15 +25,13 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-    Product.fetchAll()
-        .then(([rows, fieldData]) => {
-            res.render('shop/index', {
-                prods: rows,
-                pageTitle: 'Shop',
-                path: '/'
-            });
-        })
-        .catch(e => console.log(e));
+    Product.findAll().then(products => {
+        res.render('shop/index', {
+            prods: products,
+            pageTitle: 'Shop',
+            path: '/'
+        });
+    }).catch(e => console.log(e));
 };
 
 exports.getCart = (req, res, next) => {
