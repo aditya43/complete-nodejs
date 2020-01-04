01dcd81fb831a31b97d8287315ecd071f4ee6392
Aditya Hajare <aditya43@gmail.com>
2020-01-04 11:20:41 +0530

    Shop App - MySQL | Fetch all products

--- 13-Shop-App-MySQL/app.js
+++ 13-Shop-App-MySQL/app.js
@@ -8,11 +8,6 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 const db = require('./util/database');
 
-// Sample DB query
-db.execute('SELECT * FROM products')
-    .then(res => console.log(res))
-    .catch(e => console.log(e));
-
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -20,8 +15,6 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-
-db.execute('SELECT * FROM products');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
--- 13-Shop-App-MySQL/controllers/shop.js
+++ 13-Shop-App-MySQL/controllers/shop.js
@@ -23,13 +23,15 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-    Product.fetchAll(products => {
-        res.render('shop/index', {
-            prods: products,
-            pageTitle: 'Shop',
-            path: '/'
-        });
-    });
+    Product.fetchAll()
+        .then(([rows, fieldData]) => {
+            res.render('shop/product-list', {
+                prods: rows,
+                pageTitle: 'All Products',
+                path: '/products'
+            });
+        })
+        .catch(e => console.log(e));
 };
 
 exports.getCart = (req, res, next) => {
