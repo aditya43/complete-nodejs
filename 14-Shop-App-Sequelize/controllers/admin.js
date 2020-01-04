64ed5caef01e37bbd6171029dbec0c3c7c0024e0
Aditya Hajare <aditya43@gmail.com>
2020-01-04 16:32:24 +0530

    Shop App - Sequelize | Edit product

--- 14-Shop-App-Sequelize/controllers/admin.js
+++ 14-Shop-App-Sequelize/controllers/admin.js
@@ -25,34 +25,33 @@ exports.getEditProduct = (req, res, next) => {
         return res.redirect('/');
     }
     const prodId = req.params.productId;
-    Product.findById(prodId, product => {
-        if (!product) {
-            return res.redirect('/');
-        }
-        res.render('admin/edit-product', {
-            pageTitle: 'Edit Product',
-            path: '/admin/edit-product',
-            editing: editMode,
-            product: product
-        });
-    });
+    Product.findOne({ id: prodId })
+        .then(product => {
+            if (!product) {
+                return res.redirect('/');
+            }
+            res.render('admin/edit-product', {
+                pageTitle: 'Edit Product',
+                path: '/admin/edit-product',
+                editing: editMode,
+                product: product
+            });
+        }).catch(e => console.log(e));
 };
 
 exports.postEditProduct = (req, res, next) => {
-    const prodId = req.body.productId;
-    const updatedTitle = req.body.title;
-    const updatedPrice = req.body.price;
-    const updatedImageUrl = req.body.imageUrl;
-    const updatedDesc = req.body.description;
-    const updatedProduct = new Product(
-        prodId,
-        updatedTitle,
-        updatedImageUrl,
-        updatedDesc,
-        updatedPrice
-    );
-    updatedProduct.save();
-    res.redirect('/admin/products');
+    Product.update({
+        title: req.body.title,
+        price: req.body.price,
+        imageUrl: req.body.imageUrl,
+        description: req.body.description
+    }, {
+        where: { id: req.body.productId },
+        limit: 1
+    })
+        .then(() => {
+            res.redirect('/admin/products');
+        }).catch(e => console.log(e));
 };
 
 exports.getProducts = (req, res, next) => {
