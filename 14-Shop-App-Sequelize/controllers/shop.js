81dec6bb1156a671175049280049cb44474ca4cd
Aditya Hajare <aditya43@gmail.com>
2020-01-04 15:47:38 +0530

    Shop App - Sequelize | Get product and UI fix for large images

--- 14-Shop-App-Sequelize/controllers/shop.js
+++ 14-Shop-App-Sequelize/controllers/shop.js
@@ -13,11 +13,11 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
     const prodId = req.params.productId;
-    Product.findById(prodId)
-        .then(([product]) => {
+    Product.findOne({ id: prodId })
+        .then(product => {
             res.render('shop/product-detail', {
-                product: product[0],
-                pageTitle: product[0].title,
+                product: product,
+                pageTitle: product.title,
                 path: '/products'
             });
         })
