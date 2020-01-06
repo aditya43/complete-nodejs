07f13b1c529a96c1315bf0edcccd8136f8a21a06
Aditya Hajare <aditya43@gmail.com>
2020-01-06 19:04:33 +0530

    Project Setup | Fixed controllers indentations

--- 16-Project-Setup/src/controllers/auth.js
+++ 16-Project-Setup/src/controllers/auth.js
@@ -9,48 +9,48 @@ const privateKey = config.key.privateKey;
 const tokenExpireInMinutes = config.key.tokenExpireInMinutes;
 
 exports.authenticate = function(req, res) {
-  User.findOne({ email: req.body.email })
-  .exec(function(err, user) {
-    if (err) throw err;
+    User.findOne({ email: req.body.email })
+    .exec(function(err, user) {
+        if (err) throw err;
 
-    if (!user) {
-      response.sendUnauthorized(res, 'Authentication failed.');
-    } else if (user) {
-      user.verifyPassword(req.body.password, function(err, isMatch) {
-        if (isMatch) {
-          const token = jwt.sign(user.getTokenData(), privateKey, {
-            expiresIn: tokenExpireInMinutes
-          });
+        if (!user) {
+            response.sendUnauthorized(res, 'Authentication failed.');
+        } else if (user) {
+            user.verifyPassword(req.body.password, function(err, isMatch) {
+                if (isMatch) {
+                    const token = jwt.sign(user.getTokenData(), privateKey, {
+                        expiresIn: tokenExpireInMinutes
+                    });
 
-          res.json({
-            success: true,
-            message: 'Token created.',
-            token: token
-          });
-        } else {
-          response.sendUnauthorized(res, 'Authentication failed.');
+                    res.json({
+                        success: true,
+                        message: 'Token created.',
+                        token: token
+                    });
+                } else {
+                    response.sendUnauthorized(res, 'Authentication failed.');
+                }
+            });
         }
-      });
-    }
-  });
+    });
 }
 
 exports.verifyToken = function(req, res, next) {
-  const token = req.body.token || req.query.token || req.headers['x-access-token'];
+    const token = req.body.token || req.query.token || req.headers['x-access-token'];
 
-  if (token) {
-    jwt.verify(token, privateKey, function(err, decoded) {
-      if (err) {
-        response.sendUnauthorized(res, 'Failed to authenticate token.');
-      } else {
-        User.findById(decoded.id, function(err, user) {
-          if (err) res.send(err);
-          req.currentUser = user;
-          next();
+    if (token) {
+        jwt.verify(token, privateKey, function(err, decoded) {
+            if (err) {
+                response.sendUnauthorized(res, 'Failed to authenticate token.');
+            } else {
+                User.findById(decoded.id, function(err, user) {
+                    if (err) res.send(err);
+                    req.currentUser = user;
+                    next();
+                });
+            }
         });
-      }
-    });
-  } else {
-    response.sendUnauthorized(res, 'No token provided.');
-  }
+    } else {
+        response.sendUnauthorized(res, 'No token provided.');
+    }
 };
