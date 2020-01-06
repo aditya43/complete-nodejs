09e09b4aaf6410f9936d94d0794131b7d811cc60
Aditya Hajare <aditya43@gmail.com>
2020-01-06 19:03:51 +0530

    Project Setup | Fixed db indentations

--- 16-Project-Setup/src/db/db.js
+++ 16-Project-Setup/src/db/db.js
@@ -6,7 +6,7 @@ Mongoose.connect(config.database.url, config.database.properties);
 const db = Mongoose.connection;
 db.on('error', console.error.bind(console, 'Connection error.'));
 db.once('open', function callback() {
-  console.log("Connection with database succeeded.");
+    console.log("Connection with database succeeded.");
 });
 
 exports.db = db;
