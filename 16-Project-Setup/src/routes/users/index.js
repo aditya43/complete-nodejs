9278ab7bc1431eec7d85124ae0adcf22e893e702
Aditya Hajare <aditya43@gmail.com>
2020-01-06 19:01:45 +0530

    Project Setup | Fixed routes indentations

--- 16-Project-Setup/src/routes/users/index.js
+++ 16-Project-Setup/src/routes/users/index.js
@@ -9,13 +9,13 @@ const routes  = express.Router();
 routes.use('/:userId/items', users.loadUser, items);
 
 routes.route('/:id')
-  .all(auth.verifyToken)
-  .get(users.read)
-  .put(users.update)
-  .delete(users.delete);
+    .all(auth.verifyToken)
+    .get(users.read)
+    .put(users.update)
+    .delete(users.delete);
 
 routes.route('/')
-  .get(auth.verifyToken, users.list)
-  .post(users.create);
+    .get(auth.verifyToken, users.list)
+    .post(users.create);
 
 module.exports = routes;
--- 16-Project-Setup/src/routes/users/items.js
+++ 16-Project-Setup/src/routes/users/items.js
@@ -8,12 +8,12 @@ const routes  = express.Router({ mergeParams: true });
 routes.use(auth.verifyToken);
 
 routes.route('/')
-  .get(items.list)
-  .post(items.create);
+    .get(items.list)
+    .post(items.create);
 
 routes.route('/:id')
-  .get(items.read)
-  .put(items.update)
-  .delete(items.delete);
+    .get(items.read)
+    .put(items.update)
+    .delete(items.delete);
 
 module.exports = routes;
