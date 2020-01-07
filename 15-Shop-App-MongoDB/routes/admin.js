ffcff28e39786e7bb28f5dc1ecc60370102f3df0
Aditya Hajare <aditya43@gmail.com>
2020-01-07 15:34:16 +0530

    Shop App MongoDB | Adit get products and edit products

--- 15-Shop-App-MongoDB/routes/admin.js
+++ 15-Shop-App-MongoDB/routes/admin.js
@@ -5,8 +5,8 @@ const router = express.Router();
 
 router.get('/add-product', adminController.getAddProduct);
 router.post('/add-product', adminController.postAddProduct);
-// router.get('/products', adminController.getProducts);
-// router.get('/edit-product/:productId', adminController.getEditProduct);
+router.get('/products', adminController.getProducts);
+router.get('/edit-product/:productId', adminController.getEditProduct);
 // router.post('/edit-product', adminController.postEditProduct);
 // router.post('/delete-product', adminController.postDeleteProduct);
 
