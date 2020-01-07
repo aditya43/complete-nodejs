065dfc16a6100e1bf977d661e6a70de05a1045a8
Aditya Hajare <aditya43@gmail.com>
2020-01-07 18:22:31 +0530

    Shop App MongoDB | Add products to cart

--- 15-Shop-App-MongoDB/models/user.js
+++ 15-Shop-App-MongoDB/models/user.js
@@ -2,9 +2,26 @@ const mongo = require('../util/database');
 const { ObjectId } = require('mongodb');
 
 class User {
-    constructor(name, email) {
+    constructor(name, email, cart, id) {
         this.name = name;
         this.email = email;
+        this.cart = cart; // {items: []}
+        this._id = id;
+    }
+
+    async addToCart(product) {
+        try {
+            const db = await mongo.getInstance();
+            // const cartProduct = this.cart.items.findIndex(cp => cp._id == product._id);
+            const updatedCart = { items: [{ ...product, quantity: 1 }] };
+            await db.collection('users').updateOne(
+                { _id: ObjectId(this._id) },
+                { $set: { cart: updatedCart } }
+            );
+        } catch (e) {
+            console.log(e);
+            return false;
+        }
     }
 
     async save() {
