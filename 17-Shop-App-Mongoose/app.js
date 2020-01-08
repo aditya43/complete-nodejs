15ecca793fdffdc88055d2a0bdce9fb623a9e319
Aditya Hajare <aditya43@gmail.com>
2020-01-08 19:03:25 +0530

    Shop App Mongoose | Removed middleware to add user in req object

--- 17-Shop-App-Mongoose/app.js
+++ 17-Shop-App-Mongoose/app.js
@@ -20,11 +20,11 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(async (req, res, next) => {
-    const user = await User.findById('5e1475339869261e185c0cec');
-    req.user = await new User(user.name, user.email, user.cart, user._id);
-    next();
-});
+// app.use(async (req, res, next) => {
+//     const user = await User.findById('5e1475339869261e185c0cec');
+//     req.user = await new User(user.name, user.email, user.cart, user._id);
+//     next();
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
