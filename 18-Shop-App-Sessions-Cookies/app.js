d8a01430e8adf4dfb3288d1e55946ca179219a1f
Aditya Hajare <aditya43@gmail.com>
2020-01-09 17:42:51 +0530

    Shop App - Session & Cookies | Project Setup

---
+++ 18-Shop-App-Sessions-Cookies/app.js
@@ -0,0 +1,50 @@
+require('./util/helpers');
+require('./util/loadEnv');
+
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const User = require('./models/user');
+
+const errorController = require('./controllers/error');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(async (req, res, next) => {
+    // Create user
+    const existingUser = await User.findOne();
+
+    if (!existingUser) {
+        await new User({
+            name: 'Aditya Hajare',
+            email: 'aditya@hajare.com',
+            cart: {
+                items: []
+            }
+        }).save();
+    }
+    next();
+});
+
+app.use(async (req, res, next) => {
+    const user = await User.findOne({email: 'aditya@hajare.com'});
+    req.user = user;
+    next();
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+app.listen(process.env.PORT, () => console.log(`Server is running on ${process.env.PORT}`));
---
+++ 18-Shop-App-Sessions-Cookies/models/order.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const orderSchema = mongoose.Schema({
+    products: [{
+        product: {
+            // type: mongoose.Schema.Types.ObjectId,
+            type: Object,
+            required: true
+        },
+        quantity: {
+            type: Number,
+            required: true,
+            trim: true
+        }
+    }],
+    user: {
+        name: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'User'
+        }
+    }
+});
+
+module.exports = mongoose.model('Order', orderSchema);
+
+// const Sequelize = require('sequelize');
+
+// const sequelize = require('../util/database');
+
+// const Order = sequelize.define('order', {
+//     id: {
+//         type: Sequelize.INTEGER,
+//         autoIncrement: true,
+//         allowNull: false,
+//         primaryKey: true
+//     }
+// });
+
+// module.exports = Order;
