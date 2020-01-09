7d122654a67fc3cf180a3b3f4d4c1bfedac21f99
Aditya Hajare <aditya43@gmail.com>
2020-01-09 17:53:48 +0530

    Shop App - Session & Cookies | Login form

--- 18-Shop-App-Sessions-Cookies/app.js
+++ 18-Shop-App-Sessions-Cookies/app.js
@@ -16,6 +16,7 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -44,6 +45,7 @@ app.use(async (req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
---
+++ 18-Shop-App-Sessions-Cookies/controllers/auth.js
@@ -0,0 +1,6 @@
+exports.getLogin = (req, res, next) => {
+    res.render('auth/login', {
+        pageTitle: 'Login',
+        path: '/login'
+    });
+}
