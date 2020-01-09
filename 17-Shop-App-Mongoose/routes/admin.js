60716c3a0d29449874f865b19163d218a055fbbc
Aditya Hajare <aditya43@gmail.com>
2020-01-09 07:42:53 +0530

    Shop App Mongoose | Admin get edit product

--- 17-Shop-App-Mongoose/routes/admin.js
+++ 17-Shop-App-Mongoose/routes/admin.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get('/add-product', adminController.getAddProduct);
 router.post('/add-product', adminController.postAddProduct);
 router.get('/products', adminController.getProducts);
-// router.get('/edit-product/:productId', adminController.getEditProduct);
+router.get('/edit-product/:productId', adminController.getEditProduct);
 // router.post('/edit-product', adminController.postEditProduct);
 // router.post('/delete-product', adminController.postDeleteProduct);
 
