cc4f90a5940c2d609c8744e144ee05baa1f2a1f7
Aditya Hajare <aditya43@gmail.com>
2020-01-11 16:49:48 +0530

    Shop App - Token Authentication | Reset password form

--- 20-Shop-App-Token-Authentication/controllers/auth.js
+++ 20-Shop-App-Token-Authentication/controllers/auth.js
@@ -79,3 +79,19 @@ exports.postSignup = async (req, res, next) => {
 
     res.redirect('/login');
 }
+
+exports.getResetPassword = async (req, res, next) => {
+    let message = req.flash('error');
+
+    if (message.length > 0) {
+        message = message[0];
+    } else {
+        message = null;
+    }
+
+    res.render('auth/reset', {
+        pageTitle: 'Reset Password',
+        path: '/reset-password',
+        errorMessage: message
+    });
+}
--- 20-Shop-App-Token-Authentication/routes/auth.js
+++ 20-Shop-App-Token-Authentication/routes/auth.js
@@ -8,5 +8,6 @@ router.post('/login', authController.postLogin);
 router.post('/logout', authController.postLogout);
 router.get('/signup', authController.getSignup);
 router.post('/signup', authController.postSignup);
+router.get('/reset-password', authController.getResetPassword);
 
 module.exports = router;
