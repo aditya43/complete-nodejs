a5f9f2000999ebcbd8f29d4a29ab9a9f055912ef
Aditya Hajare <aditya43@gmail.com>
2020-01-11 16:46:59 +0530

    Shop App - Token Authentication | Reset password

---
+++ 20-Shop-App-Token-Authentication/controllers/auth.js
@@ -0,0 +1,81 @@
+const bcrypt = require('bcryptjs');
+const User = require('../models/user');
+
+exports.getLogin = (req, res, next) => {
+    let message = req.flash('error');
+
+    if (message.length > 0) {
+        message = message[0];
+    } else {
+        message = null;
+    }
+
+    res.render('auth/login', {
+        pageTitle: 'Login',
+        path: '/login',
+        errorMessage: message
+    });
+};
+
+exports.postLogin = async (req, res, next) => {
+    const password = req.body.password;
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user) {
+        req.flash('error', 'Invalid email or password');
+        return res.redirect('/login');
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+        req.flash('error', 'Invalid email or password');
+        return res.redirect('/login');
+    }
+
+    req.session.user = user;
+    req.session.isAuthenticated = true;
+
+    await req.session.save();
+    res.redirect('/');
+}
+
+exports.postLogout = async (req, res, next) => {
+    req.session.destroy(err => {
+        console.log(err);
+        res.redirect('/');
+    })
+}
+
+exports.getSignup = async (req, res, next) => {
+    let message = req.flash('error');
+
+    if (message.length > 0) {
+        message = message[0];
+    } else {
+        message = null;
+    }
+
+    res.render('auth/signup', {
+        pageTitle: 'Signup',
+        path: '/signup',
+        errorMessage: message
+    });
+}
+
+exports.postSignup = async (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+        req.flash('error', 'E-mail address already in use.');
+        return res.redirect('/signup')
+    }
+
+    const user = await new User({ email, password, cart: { items: [] } });
+    await user.save();
+
+    res.redirect('/login');
+}
