b6e3ffe0921f4da25894ea8168715dd1841ccfa0
Aditya Hajare <aditya43@gmail.com>
2020-01-12 08:55:44 +0530

    Shop App Authentication | Admin controller fixed eslint errors

--- 20-Shop-App-Token-Authentication/controllers/admin.js
+++ 20-Shop-App-Token-Authentication/controllers/admin.js
@@ -4,7 +4,7 @@ exports.getAddProduct = (req, res, next) => {
     res.render('admin/edit-product', {
         pageTitle: 'Add Product',
         path: '/admin/add-product',
-        editing: false,
+        editing: false
     });
 };
 
@@ -42,7 +42,7 @@ exports.getEditProduct = async (req, res, next) => {
         pageTitle: 'Edit Product',
         path: '/admin/edit-product',
         editing: editMode,
-        product: product,
+        product: product
     });
 };
 
@@ -81,7 +81,7 @@ exports.getProducts = async (req, res, next) => {
     res.render('admin/products', {
         prods: products,
         pageTitle: 'Admin Products',
-        path: '/admin/products',
+        path: '/admin/products'
     });
 };
 
