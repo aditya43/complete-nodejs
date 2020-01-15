8b3b8458d90a66444ecb465532e01eaa5e804bd6
Aditya Hajare <aditya43@gmail.com>
2020-01-15 15:06:35 +0530

    Blog | Allow CORS for REST APIs

--- 26-Blog/app.js
+++ 26-Blog/app.js
@@ -8,6 +8,13 @@ const feedRoutes = require('./routes/feed');
 const app = express();
 
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+    // CORS Middleware
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+})
 
 app.use('/feed', feedRoutes);
 
