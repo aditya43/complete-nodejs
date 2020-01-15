dc4d858dfe515f0e72ae533a279257b6027cd0e5
Aditya Hajare <aditya43@gmail.com>
2020-01-15 18:47:41 +0530

    ReactJS Social Network | CORS Fix

--- 26-ReactJS-Social-Network-Backend/app.js
+++ 26-ReactJS-Social-Network-Backend/app.js
@@ -8,11 +8,11 @@ const feedRoutes = require('./routes/feed');
 const app = express();
 
 app.use(bodyParser.json());
-app.use((req, res, next) => {
+app.use(function(req, res, next) {
     // CORS Middleware
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Authorization');
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Authorization');
     next();
 })
 
