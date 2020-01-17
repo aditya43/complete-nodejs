502b6c05c534d1e24ae46b04f61804ecac45add7
Aditya Hajare <aditya43@gmail.com>
2020-01-17 22:25:43 +0530

    React JS Social Network | Post model

---
+++ 26-ReactJS-Social-Network-Backend/models/post.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+    const post = sequelize.define('post', {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: true
+        },
+        imageUrl: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        creator: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        }
+    }, {
+        sequelize,
+        timestamps: true,
+        modelName: 'Post'
+    });
+
+    post.associate = function (models) {
+    // associations can be defined here
+    };
+
+    return post;
+};
