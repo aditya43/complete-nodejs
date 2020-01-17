2e0c40ff9b6162c3eeb784dadd06fefbd10d60ff
Aditya Hajare <aditya43@gmail.com>
2020-01-17 22:02:01 +0530

    React JS Social Network | Add sequelize

--- 26-ReactJS-Social-Network-Backend/app.js
+++ 26-ReactJS-Social-Network-Backend/app.js
@@ -10,13 +10,6 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
-// app.use(function(req, res, next) {
-//     // CORS Middleware
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Authorization');
-//     next();
-// })
 
 app.use('/feed', feedRoutes);
 
