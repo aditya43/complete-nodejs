15d3565eeb4bea8a2bc05c55611c3157043e5c68
Aditya Hajare <aditya43@gmail.com>
2020-01-18 21:21:35 +0530

    React JS Social Network | User model

---
+++ 26-ReactJS-Social-Network-Backend/migrations/20200118153438-create-user.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('users', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            email: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            password: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            name: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            status: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable('users');
+    }
+};
---
+++ 26-ReactJS-Social-Network-Backend/models/user.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+    const user = sequelize.define('user', {
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        status: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    }, {
+        sequelize,
+        timestamps: true,
+        modelName: 'User'
+    });
+
+    user.associate = function (models) {
+        user.hasMany(models.post, {
+            foreignKey: 'creator'
+        });
+    };
+
+    return user;
+};
