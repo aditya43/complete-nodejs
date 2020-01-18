877eb3b188e1dd7e61a0e297d554f67a069977ee
Aditya Hajare <aditya43@gmail.com>
2020-01-18 17:20:34 +0530

    React JS Social Network | Multer config

--- 26-ReactJS-Social-Network-Backend/app.js
+++ 26-ReactJS-Social-Network-Backend/app.js
@@ -1,12 +1,16 @@
 require('./utils/loadEnv');
 
+// Middlewares
 const errorLogger = require('./middleware/errorLogger');
+const multer = require('./middleware/multer');
 
+// Express config
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+// Route
 const feedRoutes = require('./routes/feed');
 
 const app = express();
@@ -14,9 +18,11 @@ const app = express();
 app.use(bodyParser.json());
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(cors());
+app.use(multer);
 
 app.use('/feed', feedRoutes);
 
 app.use(errorLogger);
 
+// Boot up
 app.listen(process.env.PORT, () => console.log(`Server is running on ${process.env.PORT}`));
