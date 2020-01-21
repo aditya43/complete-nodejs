d5e417bb236eaa914366c4bdabfedc7c042f602a
Aditya Hajare <aditya43@gmail.com>
2020-01-21 10:25:48 +0530

    GraphQL | ReactJS - Using GraphQL Query Variables - Fetch single post

--- 30-ReactJS-Social-Network-Frontend-GraphQL/src/pages/Feed/SinglePost/SinglePost.js
+++ 30-ReactJS-Social-Network-Frontend-GraphQL/src/pages/Feed/SinglePost/SinglePost.js
@@ -4,76 +4,79 @@ import Image from '../../../components/Image/Image';
 import './SinglePost.css';
 
 class SinglePost extends Component {
-  state = {
-    title: '',
-    author: '',
-    date: '',
-    image: '',
-    content: ''
-  };
-
-  componentDidMount() {
-    const postId = this.props.match.params.postId;
-    const graphqlQuery = {
-      query: `
-        {
-          post(id: "${postId}") {
-            id
-            title
-            content
-            imageUrl
-            creator{
-              id
-              name
-              email
-            }
-            createdAt
-          }
-        }
-      `
+    state = {
+        title: '',
+        author: '',
+        date: '',
+        image: '',
+        content: ''
     };
-    fetch('http://localhost:8080/graphql', {
-      method: 'POST',
-      body: JSON.stringify(graphqlQuery),
-      headers: {
-        Authorization: `Bearer ${this.props.token}`,
-        'Content-Type': 'application/json'
-      }
+
+    componentDidMount() {
+        const postId = this.props.match.params.postId;
+        const graphqlQuery = {
+            query: `
+                query FetchSinglePost ($postId: ID!) {
+                    post(id: $postId) {
+                        id
+                        title
+                        content
+                        imageUrl
+                        creator{
+                            id
+                            name
+                            email
+                        }
+                        createdAt
+                    }
+                }
+            `,
+            variables: {
+                postId
+            }
+        };
+        fetch('http://localhost:8080/graphql', {
+        method: 'POST',
+        body: JSON.stringify(graphqlQuery),
+        headers: {
+            Authorization: `Bearer ${this.props.token}`,
+            'Content-Type': 'application/json'
+        }
     })
-      .then(res => {
+    .then(res => {
         return res.json();
-      })
-      .then(resData => {
+    })
+    .then(resData => {
         if(resData.errors) {
             throw new Error('Post not found')
         }
         this.setState({
-          title: resData.data.post.title,
-          author: resData.data.post.creator.name,
-          image: 'http://localhost:8080/' + resData.data.post.imageUrl,
-          date: new Date(resData.data.post.createdAt).toLocaleDateString('en-US'),
-          content: resData.data.post.content
+            title: resData.data.post.title,
+            author: resData.data.post.creator.name,
+            image: 'http://localhost:8080/' + resData.data.post.imageUrl,
+            date: new Date(resData.data.post.createdAt).toLocaleDateString('en-US'),
+            content: resData.data.post.content
         });
-      })
-      .catch(err => {
+    })
+    .catch(err => {
         console.log(err);
-      });
-  }
+    });
+}
 
-  render() {
+render() {
     return (
-      <section className="single-post">
+        <section className="single-post">
         <h1>{this.state.title}</h1>
         <h2>
-          Created by {this.state.author} on {this.state.date}
+        Created by {this.state.author} on {this.state.date}
         </h2>
         <div className="single-post__image">
-          <Image contain imageUrl={this.state.image} />
+        <Image contain imageUrl={this.state.image} />
         </div>
         <p>{this.state.content}</p>
-      </section>
-    );
-  }
+        </section>
+        );
+    }
 }
 
 export default SinglePost;
